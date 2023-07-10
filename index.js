53f079da6d725f3e30204da7299c5769939ed2e6
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-11 01:51:07 +0300

    Mission Passed

--- index.js
+++ index.js
@@ -1,66 +1,61 @@
 /* Your Code Here */
 
-  let createEmployeeRecord = function(employeeData) {
-    let testEmployee = {};
-    testEmployee.firstName = employeeData[0];
-    testEmployee.familyName = employeeData[1];
-    testEmployee.title = employeeData[2];
-    testEmployee.payPerHour = employeeData[3];
-    testEmployee.timeInEvents = [];
-    testEmployee.timeOutEvents = [];
-    return testEmployee;
-  }
+let createEmployeeRecord = function (employeeData) {
+  let testEmployee = {};
+  testEmployee.firstName = employeeData[0];
+  testEmployee.familyName = employeeData[1];
+  testEmployee.title = employeeData[2];
+  testEmployee.payPerHour = employeeData[3];
+  testEmployee.timeInEvents = [];
+  testEmployee.timeOutEvents = [];
+  return testEmployee;
+};
 function createEmployeeRecords(twoRows) {
-  return twoRows.map(createEmployeeRecord)
-  }
+  return twoRows.map(createEmployeeRecord);
+}
 
 let twoRows = [
   ["moe", "sizlak", "barkeep", 2],
   ["bartholomew", "simpson", "scamp", 3],
 ];
 
-
-
-function createTimeInEvent( timestamp) {
-  let [date, hour] = timestamp.split(' ')
+function createTimeInEvent(timestamp) {
+  let [date, hour] = timestamp.split(" ");
   this.timeInEvents.push({
     type: "TimeIn",
-    hour : parseInt(hour, 10),
+    hour: parseInt(hour, 10),
     date,
   });
- 
 
   return this;
 }
 
 function createTimeOutEvent(timestamp) {
-  let [date, hour] = timestamp.split(' ')
+  let [date, hour] = timestamp.split(" ");
   this.timeOutEvents.push({
     type: "TimeOut",
-    hour : parseInt(hour, 10),
+    hour: parseInt(hour, 10),
     date,
-  })
- 
+  });
+
   return this;
 }
 
 function hoursWorkedOnDate(soughtDate) {
-  let timeInEvent = this.timeInEvents.find(e => e.date === soughtDate);
-  let timeOutEvent = this.timeOutEvents.find(e => e.date === soughtDate);
-
+  let timeInEvent = this.timeInEvents.find((e) => e.date === soughtDate);
+  let timeOutEvent = this.timeOutEvents.find((e) => e.date === soughtDate);
 
   const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
 
   return hoursWorked;
 }
 
-let wagesEarnedOnDate = function(dateSought) {
-  const wagesEarned = hoursWorkedOnDate.call(this, dateSought) * this.payPerHour
+let wagesEarnedOnDate = function (dateSought) {
+  const wagesEarned =
+    hoursWorkedOnDate.call(this, dateSought) * this.payPerHour;
 
   return parseFloat(wagesEarned.toString());
-}
- 
- 
+};
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -79,22 +74,21 @@ let allWagesFor = function () {
   let payable = eligibleDates.reduce(
     function (memo, d) {
       return memo + wagesEarnedOnDate.call(this, d);
-    }.bind(this), 0); // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    }.bind(this),
+    0
+  ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
   return payable;
- 
-  }
+};
 
-  let findEmployeeByFirstName = function(srcArray, firstName) {
+let findEmployeeByFirstName = function (srcArray, firstName) {
+  return srcArray.find(function (rec) {
+    return rec.firstName === firstName;
+  });
+};
 
-    return srcArray.find(function(rec){
-      return rec.firstName === firstName
-    });
-  };
-
-  let calculatePayroll = function(arrayOfEmployeeRecords){
-    return arrayOfEmployeeRecords.reduce(function(memo, rec){
-      return memo + allWagesFor.call(rec)
-    }, 0);
-  };
-  
+let calculatePayroll = function (arrayOfEmployeeRecords) {
+  return arrayOfEmployeeRecords.reduce(function (memo, rec) {
+    return memo + allWagesFor.call(rec);
+  }, 0);
+};
